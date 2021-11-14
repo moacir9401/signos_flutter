017ff6aeba5f530ab6db1bbab06e873722d508de
Moacir Afonso <36249463+moacir9401@users.noreply.github.com>
2021-11-13 21:28:18 -0300

    implementando a listaSignos e dadosSignos

---
+++ lib/pages/ListaSignos.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:signos/utils/cardSigno.dart';
+import '../data/dadosSignos.dart' as colecao_signos;
+
+class ListaSignos extends StatelessWidget{
+
+  var signos = colecao_signos.signos;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Características dos Signos"),
+          centerTitle: true,
+        ),
+        body:GridView.count(
+          padding: EdgeInsets.only(top: 20),
+          crossAxisCount: 2,
+          children: List.generate(
+              signos.length,
+              (index) => CardSigno(signos[index])
+          ),
+        )
+    );
+  }
+
+}
---
+++ lib/utils/cardSigno.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class CardSigno extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+
+    );
+  }
+}
